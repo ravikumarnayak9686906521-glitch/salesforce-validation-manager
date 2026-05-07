8d407db8ba24fc74f5f6618ac2fdaf685a2fbad7
Ravikumar <rn5127610@gmail.com>
2026-05-07 09:01:06 +0530

    Fix duplicate SalesforceConfig bean

--- src/main/java/com/salesforce/manager/SalesforceValidationManagerApplication.java
+++ src/main/java/com/salesforce/manager/SalesforceValidationManagerApplication.java
@@ -2,12 +2,8 @@ package com.salesforce.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.salesforce.manager.config.SalesforceConfig;
 
 @SpringBootApplication
-@EnableConfigurationProperties(SalesforceConfig.class)
 public class SalesforceValidationManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(SalesforceValidationManagerApplication.class, args);
